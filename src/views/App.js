d3e1f6e7be8fe3ccf8f0775510debb965e312e00
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2023-11-17 14:57:43 +0100

    test another branch

--- src/views/App.js
+++ src/views/App.js
@@ -7,7 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit MOja <code>src/App.js</code> and save to reload.
+          Edit MOja <code>src/App.js</code> and save to reload. Edit MOja{' '}
+          <code>src/App.js</code> and save to reload. Edit MOja{' '}
+          <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Edit MOja <code>src/App.js</code> and save to reload. Edit MOja{' '}
+          <code>src/App.js</code> and save to reload. Edit MOja{' '}
+          <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
