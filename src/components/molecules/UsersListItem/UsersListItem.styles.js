5d3edaf2dac41f291b4e9053967296e143ba1f18
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2023-12-01 22:25:44 +0100

    styling usersList

---
+++ src/components/molecules/UsersListItem/UsersListItem.styles.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: lightgrey;
+  }
+`;
+
+export const StyledAverage = styled.div`
+  width: 34px;
+  height: 34px;
+  border-radius: 50px;
+  background-color: ${({ average, theme }) => {
+    console.log(average, theme);
+    let averageNumber = parseFloat(average);
+    if (averageNumber <= 2) return theme.colors.error;
+    if (averageNumber <= 5) return theme.colors.warning;
+    if (averageNumber <= 10) return theme.colors.success;
+  }};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${({ theme }) => theme.fontSize.m};
+  margin-right: 24px;
+`;
+
+export const StyledInfo = styled.div`
+  padding: 25px 20px;
+
+  p {
+    margin: 0;
+    color: ${({ theme }) => theme.colors.darkGrey};
+  }
+
+  p:first-child {
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+    font-size: ${({ theme }) => theme.fontSize.l};
+  }
+
+  p:last-child {
+    font-size: ${({ theme }) => theme.fontSize.s};
+  }
+`;
---
+++ src/components/organisms/UsersList/UsersList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { users } from 'data/users';
+import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  width: 80%;
+  max-width: 500px;
+  padding: 40px 30px;
+  border-radius: 25px;
+  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.8);
+`;
+
+const UsersList = () => (
+  <Wrapper>
+    <ul>
+      {users.map((userData) => (
+        <UsersListItem userData={userData} />
+      ))}
+    </ul>
+  </Wrapper>
+);
+
+export default UsersList;
