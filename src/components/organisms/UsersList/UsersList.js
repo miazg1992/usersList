b78314062b8c3965abd3730bbf467a712c3b6db3
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2023-12-15 14:43:16 +0100

    useState in userList

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { users } from 'data/users';
+import { users as usersData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 console.log('i');
 
@@ -14,47 +14,42 @@ const Wrapper = styled.div`
   box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.8);
 `;
 
-class UsersList extends React.Component {
-  state = {
-    listTitle: `User's list`,
-    counter: 0,
-    users,
+const UsersList = (props) => {
+  const [counter, setCounter] = useState(0);
+  const [users, setUsers] = useState(usersData);
+
+  // const getUsers = () => {
+  //   setUsers(usersData);
+  // };
+
+  const changeTitle = () => {
+    setCounter(counter + 1);
   };
 
-  changeTitle = () => {
-    console.log('zmieniam');
-    this.setState((prevState) => ({
-      counter: prevState.counter + 1,
-    }));
+  const deleteUsers = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
   };
 
-  deleteUsers = (name) => {
-    console.log(name);
-    const filteredUsers = this.state.users.filter((user) => user.name !== name);
-    console.log(filteredUsers);
-    this.setState({ users: filteredUsers });
-  };
-  render() {
-    return (
-      <Wrapper>
-        <h1>
-          {this.state.listTitle} {this.state.counter}
-          {this.props.title}
-        </h1>
-        <button onClick={this.changeTitle}>Change Title</button>
-        <ul>
-          {this.state.users.map((userData, index) => (
-            <UsersListItem
-              userData={userData}
-              index={index}
-              key={userData.name}
-              deleteUsers={this.deleteUsers}
-            />
-          ))}
-        </ul>
-      </Wrapper>
-    );
-  }
-}
+  return (
+    <Wrapper>
+      <h1>
+        {counter}
+        {props.title}
+      </h1>
+      <button onClick={changeTitle}>Change Title</button>
+      <ul>
+        {users.map((userData, index) => (
+          <UsersListItem
+            userData={userData}
+            index={index}
+            key={userData.name}
+            deleteUsers={deleteUsers}
+          />
+        ))}
+      </ul>
+    </Wrapper>
+  );
+};
 
 export default UsersList;
